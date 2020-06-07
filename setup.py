8058a41d1b96bd583557da96ace7685479a218b6
kwlee0220 <kwlee@KWLEE-OFFICE>
2020-06-07 20:52:20 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dric_client',
-    version = '1.0.6',
+    version = '1.0.7',
     description = 'Python client interface to Dr.IC platform',
     author = 'Kang-Woo Lee',
     author_email = 'kwlee@etri.re.kr',
