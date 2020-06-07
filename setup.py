3c4e4ba8043b63cd5ffe58924bc07bd6a2e2573b
kwlee0220 <kwlee@etri.re.kr>
2020-06-07 11:24:25 +0900

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dric_client',
+    version = '1.0.4',
+    description = 'Python client interface to Dr.IC platform',
+    author = 'Kang-Woo Lee',
+    author_email = 'kwlee@etri.re.kr',
+    url = 'https://github.com/kwlee0220/dric.client.python',
+    install_requires = [],
+    packages = find_packages(),
+    python_requires = '>=3',
+    zip_safe = False
+)
--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import dric.dric_client as dric
+
+from dric.client import DrICClient
 from dric.types import *
 
-dric.open_client('dric_client.yaml')
+dric = DrICClient.connect("localhost", 10703)
 
-camera_frames = dric.connect_topic(CameraFrame, "dric/camera_frames")
-bbox_tracks = dric.connect_topic(ObjectBBoxTrack, "dric/bbox_tracks")
+camera_frames = dric.get_topic(CameraFrame, "dric/camera_frames")
+bbox_tracks = dric.get_topic(ObjectBBoxTrack, "dric/bbox_tracks")
 
 def on_camera_frame(frame):
     print(frame.camera_id)
     track = ObjectBBoxTrack(frame.camera_id, "obj_01", None, 0, frame.ts)
     bbox_tracks.publish(track)
 
-camera_frames.subscribe(on_camera_frame, 1)
+camera_frames.subscribe(on_camera_frame)
