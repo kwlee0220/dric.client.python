e70e09ff53d014d1ee8b1f19230167bd266a6a5e
kwlee0220 <kwlee@KWLEE-OFFICE>
2020-08-27 10:58:38 +0900

    .

--- dric/camera.py
+++ dric/camera.py
@@ -10,6 +10,12 @@ class Resolution(namedtuple('Resolution', 'width height')):
         return '%d x %d' % self
 
 class Camera:
+    from enum import Enum
+    class State(Enum):
+        CLOSED = 1
+        OPEN = 2
+        CLOSING = 3
+
     def __init__(self, id, fps):
         self.id = id
         self.fps = fps
@@ -19,23 +25,56 @@ class Camera:
         self.size = Resolution(int(self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                                 int(self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
+        import threading
+        self.cond = threading.Condition()
+        self.state = State.CLOSED
+
     def __enter__(self):
         return self
     def __exit__(self, exc_type, exc_value, track_back):
+        with self.cond:
+            self.state = State.CLOSING
+            while self.state != State.CLOSED:
+                self.cond.wait()
         self.release()
         return False
+
     def release(self):
         self.vcap.release()
 
+    def capturing_work(self):
+        self.buffer = []
+        with self.cond:
+            self.state = State.OPEN
+
+        while True:
+            ret, frame = self.vcap.read()
+            if ret:
+                with self.cond:
+                    if self.state == State.CLOSING:
+                        self.state = State.CLOSED
+                        self.cond.notify_all()
+                        break
+
+                    self.buffer.append(namedtuple(current_millis(), frame))
+                    if len(self.buffer) > 5:
+                        del self.buffer[0:2]
+                    self.cond.notify_all()
+        self.release()
+
     def capture(self):
         started = current_millis()
-        ret, frame = self.vcap.read()
-        if ret:
-            elapsed = current_millis() - started
-            sleep_millis = self.interval - elapsed
-            return (sleep_millis,  frame)
-        else:
-            return (0, None)
+        with self.cond:
+            while True:
+                self.cond.wait()
+                if self.state != State.OPEN:
+                    return (0, None)
+
+                last = self.buffer[-1]
+                elapsed = last[0] - started
+                if elapsed >= 0:
+                    sleep_millis = self.interval - elapsed
+                    return (sleep_millis,  last[1])
 
     def __str__(self):
         return '{0}[{1},{2}]'.format(type(self).__name__, self.id, str(self.size))
